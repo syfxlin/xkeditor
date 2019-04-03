b123cce13a28d3e87688586ad606ec3f676dc1d8
syfxlin <syfxlin@gmail.com>
2019-04-04 01:07:55 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import AtComponents from 'at-ui'
+import 'at-ui-style'
+Vue.use(AtComponents)
+import marked from 'marked'
+//导入js，随处可用
+Object.defineProperty(Vue.prototype, '$marked', { value: marked });
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
