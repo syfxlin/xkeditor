2b6db265d5b05e316b695dec43722cd8d85efe82
syfxlin <syfxlin@gmail.com>
2019-04-15 01:13:20 +0000

    Remove vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <editor />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,26 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import AtComponents from 'at-ui'
 import 'at-ui-style'
 import './components/dialogDrag'
 Vue.use(AtComponents)
-import marked from 'marked'
-//导入js，随处可用
-Object.defineProperty(Vue.prototype, '$marked', { value: marked })
 
-import ACE from './components/ACE_Editor.vue'
+import ACE from '@/components/ACE_Editor'
 Vue.component('ace', ACE)
-import TinyMCE from './components/TinyMCE_Editor.vue'
+import TinyMCE from '@/components/TinyMCE_Editor'
 Vue.component('tinymce', TinyMCE)
+import Editor from '@/components/Editor'
+Vue.component('editor', Editor)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: {
     App
   },
