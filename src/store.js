a7174186e7aa17068909e4f4e760b63bfbfd6230
syfxlin <syfxlin@gmail.com>
2019-12-06 11:26:35 +0800

    feat: :sparkles: Move lang list to out setting

--- src/store.js
+++ src/store.js
@@ -67,7 +67,23 @@ const state = Vue.observable({
       pasteFormat: true,
       pasteImageUpload: true,
       enableTinyMCE: true,
-      judge0API: "https://run-code.lincdn.top"
+      judge0API: "https://run-code.lincdn.top",
+      runCodeLangList: {
+        c: 1,
+        cpp: 2,
+        bash: 3,
+        csharp: 4,
+        go: 5,
+        java: 6,
+        node: 7,
+        php: 8,
+        python: 9,
+        python2: 10,
+        ruby: 11,
+        rust: 12,
+        scala: 13,
+        typescript: 14
+      }
     }
   },
   aceEditor: null,
--- src/utils/runCode.js
+++ src/utils/runCode.js
@@ -1,27 +1,16 @@
 import runJS from "./run/runJS";
 import runJudge0 from "./run/runJudge0";
-
-const langList = {
-  c: 1,
-  cpp: 2,
-  bash: 3,
-  csharp: 4,
-  go: 5,
-  java: 6,
-  node: 7,
-  php: 8,
-  python: 9,
-  python2: 10,
-  ruby: 11,
-  rust: 12,
-  scala: 13,
-  typescript: 14
-};
+import store from "../store";
 
 export default function runCode(code, lang, input, outputEle) {
   if (lang === "javascript" || lang === "js") {
     runJS(code, outputEle);
   } else {
-    runJudge0(code, langList[lang], input, outputEle);
+    runJudge0(
+      code,
+      store.state.setting.xkSetting.runCodeLangList[lang],
+      input,
+      outputEle
+    );
   }
 }
